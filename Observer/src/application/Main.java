a038e77802840097e618946b7d3e189d60766a09
alanbarco <alaxbarc@espol.edu.ec>
2021-06-20 20:56:21 -0500

    Main modificado

--- Observer/src/application/Main.java
+++ Observer/src/application/Main.java
@@ -1,26 +1,40 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene scene;
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			scene = new Scene(loadFXML("VentanaPrincipal"), 640, 480);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		stage.setTitle("Observer AspectJ");
+        stage.setScene(scene);
+        stage.show();
 	}
 	
+	static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
